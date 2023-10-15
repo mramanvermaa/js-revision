760c567cb1fa45da557f4d64ae166005574dfe2f
Amanvrma7879 <133656778+mramanvermaa@users.noreply.github.com>
2023-10-15 13:37:14 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 121344
+let accountEmail = "aman1234@gmail.com"
+var accountPassword = "123457"
+accountcity = "jaipur"
+let accountState;
+
+//accountId = 3 not allowed
+              
+accountEmail = "amna56356@gmail.com"
+accountPassword = " 773578"
+accountcity = "indore"
+
+console.table([accountEmail, accountPassword, accountcity, accountId,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+ "use strict"; // treat all JS code as newer version 
+ 
+ // alert (4 + 4) we are using node.js, not broswer
+
+ console.log(3+
+    3) // code readablility should be high kind of proper documentaion
+     
+    console.log("aman")
+
+    let name = "aman"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => false/true
+// null => standalone value 
+// undefined
+//symbol => unique
+
+// object
+
+console.log(typeof null); // output of this is object
+console.log(typeof undefined); // output of this undefined
